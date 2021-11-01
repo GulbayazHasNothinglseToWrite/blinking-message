ac4f49dc4d9cdd8221531b188bddcdfa5880fafe
GulbayazHasNothinglseToWrite <92817865+GulbayazHasNothinglseToWrite@users.noreply.github.com>
2021-11-01 14:16:51 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,55 @@
+basic.showIcon(IconNames.Heart)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showIcon(IconNames.Heart)
+basic.showLeds(`
+    . # . # .
+    # # # # #
+    # # # # #
+    . # # # .
+    . . # . .
+    `)
+basic.showLeds(`
+    . . # . #
+    . # # # #
+    . # # # #
+    . . # # #
+    . . . # .
+    `)
+basic.showLeds(`
+    . . . # .
+    . . # # #
+    . . # # #
+    . . . # #
+    . . . . #
+    `)
+basic.showLeds(`
+    . . . . #
+    . . . # #
+    . . . # #
+    . . . . #
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    . . . . #
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showString("STOP BULLYING")
+basic.forever(function () {
+	
+})
